965ee96e4e5041ad906c1bf76c0c048b6a383e29
sravyakodi <133135698+sravyakodi@users.noreply.github.com>
2025-07-16 13:03:15 +0530

    update

--- DEmo5.java
+++ DEmo5.java
@@ -3,5 +3,9 @@ public static void main(String [] arg)
   {
 System.out.println("Centrsal to local");
     System.out.println(".......");
+    system.out.println("hai");
+      system.out.println("hello");
+      system.out.println("namaskaram");
+    
   }
 }
